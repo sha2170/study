7bfa8b74514a2a24f7f69c2879fa25a3d32c0aba
sha2170 <81471832+sha2170@users.noreply.github.com>
2024-08-27 17:12:47 +0900

    Delete src directory

--- src/src/JavaEx02/Computer.java
+++
@@ -1,9 +0,0 @@
-package JavaEx02;
-
-public class Computer extends Calculator {
-
-	public double areaCircle(double r) {
-		System.out.println("Computer areaCircle() 실행");
-		return Math.PI*r*r;
-	}
-}
--- src/src/Mission/mission1_Shape_0827.java
+++
@@ -1,5 +0,0 @@
-package Mission;
-public interface mission1_Shape_0827 {
-	// 면적을 계산하는 메서드
-	double calculate();
-}
